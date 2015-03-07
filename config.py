66c9bd158fab70d9fb4046be4d477459113ad804
Desmond Rivet <desmond.rivet@gmail.com>
2015-03-07 13:26:04 -0500

    add support for smart attributes and archives

--- config.py
+++ config.py
@@ -16,9 +16,21 @@ YAWT_TAGGING_COUNT_FILE = 'tagcounts'
 YAWT_CATEGORY_BASE = '/blog/'
 YAWT_CATEGORY_COUNT_FILE = 'categorycounts'
 
+YAWT_COLLECTIONS_SORT_FIELD = 'smart_create_time'
+YAWT_ARCHIVE_DATEFIELD = 'smart_create_time'
+YAWT_ARCHIVE_BASE = '/blog/'
+
 from whoosh.fields import TEXT, DATETIME, IDLIST, KEYWORD
 WHOOSH_INDEX_ROOT = '/home/dcr/blogging/website/_state/index'
-YAWT_WHOOSH_ARTICLE_INFO_FIELDS = {'create_time': DATETIME(sortable=True),
+YAWT_WHOOSH_ARTICLE_INFO_FIELDS = {'smart_create_time': DATETIME(sortable=True),
                                    'categories': IDLIST(),
                                    'tags': KEYWORD(commas=True)}
 YAWT_WHOOSH_ARTICLE_FIELDS = {'content': TEXT()}
+
+GIT_REPOPATH = '/home/dcr/blogging/website'
+
+YAWT_SMART_ATTRIBUTES = {
+    'smart_create_time': ['ctime', 'git_create_time', 'create_time'],
+    'smart_modified_time': ['mtime', 'git_modified_time', 'modified_time'],
+    'smart_author': ['author', 'git_author']
+}
