f1ab869fe775bbd2a0b6c2cc8e8ca9a78e6928e0
Desmond Rivet <desmond.rivet@gmail.com>
2015-03-08 17:00:01 -0400

    use ISO 8601 datetime format for markdown dates.  Fix templates for new markdown types.

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ YAWT_ARTICLE_TEMPLATE = 'article'
 YAWT_ARTICLE_EXTENSIONS = ['txt']
 YAWT_DEFAULT_EXTENSION = 'txt'
 YAWT_STATE_FOLDER = '_state'
+
 YAWT_MULTIMARKDOWN_FILE_EXTENSIONS = ['md', 'txt']
+YAWT_MULTIMARKDOWN_TYPES = {'md_create_time': 'iso8601', 
+                            'md_modified_time': 'iso8601', 
+                            'tags': 'list'}
 
 YAWT_TAGGING_BASE = '/blog/'
 YAWT_TAGGING_COUNT_FILE = 'tagcounts'
@@ -30,7 +34,7 @@ YAWT_WHOOSH_ARTICLE_FIELDS = {'content': TEXT()}
 GIT_REPOPATH = '/home/dcr/blogging/website'
 
 YAWT_SMART_ATTRIBUTES = {
-    'smart_create_time': ['ctime', 'git_create_time', 'create_time'],
-    'smart_modified_time': ['mtime', 'git_modified_time', 'modified_time'],
+    'smart_create_time': ['md_create_time', 'git_create_time', 'create_time'],
+    'smart_modified_time': ['md_modified_time', 'git_modified_time', 'modified_time'],
     'smart_author': ['author', 'git_author']
 }
