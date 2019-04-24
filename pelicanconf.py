145280c902484e1810cd44fa9e52740e423db9fe
Desmond Rivet <desmond.rivet@gmail.com>
2019-04-23 23:17:23 -0400

    add webmention endppoint config

--- pelicanconf.py
+++ pelicanconf.py
@@ -163,6 +163,7 @@ EDIT_ROOT=r'https://github.com/drivet/website/edit/master/'+PATH
 AUTH_ENDPOINT = r'https://desmondrivet.com/auth/'
 TOKEN_ENDPOINT = r'https://tokens.indieauth.com/token'
 MICROPUB_ENDPOINT = r'https://desmondrivet.com/micropub'
+WEBMENTION_ENDPOINT = r'https://desmondrivet.com/webmention'
 
 # only send mentions past this date
 WEBMENTION_DATE_FROM = ''
